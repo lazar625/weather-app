011f76c10737f9ab293d7297017ca716d08d66ca
lazar625 <klazarev625@gmail.com>
2025-03-13 23:00:06 +0300

    first

---
+++ src/main/java/by/lazar/weatherapp/Condition.java
@@ -0,0 +1,15 @@
+package by.lazar.weatherapp;
+
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+
+@JsonIgnoreProperties(ignoreUnknown = true)
+@Getter
+@Setter
+@ToString
+public class Condition {
+    private String text;
+}
---
+++ src/main/java/by/lazar/weatherapp/Weather.java
@@ -0,0 +1,18 @@
+package by.lazar.weatherapp;
+
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+
+@Getter
+@Setter
+@ToString
+@JsonIgnoreProperties(ignoreUnknown = true)
+public class Weather {
+
+    private Location location;
+    private Current current;
+
+}
